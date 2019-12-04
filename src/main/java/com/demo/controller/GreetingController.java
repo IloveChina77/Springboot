d19c1d1bafab09f20f17c692fbafe0296d8b3ef2
醉卧意逍遥 <57378028+IloveChina77@users.noreply.github.com>
2019-12-04 21:07:04 +0800

    做一些测试性修改

--- src/main/java/com/demo/controller/GreetingController.java
+++ src/main/java/com/demo/controller/GreetingController.java
@@ -6,7 +6,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
-//
+
 @Controller // 使用controller注解来标志这是一个控制器
 public class GreetingController {
 
--- src/main/java/com/demo/controller/WebSocketController.java
+++
@@ -1,8 +0,0 @@
-package com.demo.controller;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-
-public class WebSocketController {
-
-}
